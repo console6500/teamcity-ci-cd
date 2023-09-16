7e72b90a64e8ba3f8622a83a3278972cac5d1dfa
Console 6500 <73613222+console6500@users.noreply.github.com>
2023-09-15 23:02:46 -0700

    Add files via upload

--- Makefile
+++ Makefile
@@ -7,25 +7,32 @@ BUILD_NUMBER=undefined
 
 CODE=$(shell ls *.py)
 
-hello99:
+hello:
 	@echo "Here are the targets for this Makefile:"
-	@echo "  requirements  - install the project requirements"
-	@echo "  lint          - run linters on the code"
-	@echo "  black         - run black to format the code"
-	@echo "  test          - run the tests"
-	@echo "  build         - build the lambda.zip file"
-	@echo "  deploy        - deploy the lambda.zip file to AWS"
-	@echo "  clean         - remove the lambda.zip file"
-	@echo "  all           - clean, lint, black, test, build, and deploy"
+	@echo "  requirements   - install the project requirements"
+	@echo "  lint           - run linters on the code"
+	@echo "  black          - run black to format the code"
+	@echo "  test           - run the tests"
+	@echo "  build          - build the lambda.zip file"
+	@echo "  deploy         - deploy the lambda.zip file to AWS"
+	@echo "  testdeployment - test the deployment"
+	@echo "  clean          - remove the lambda.zip file"
+	@echo "  all            - clean, lint, black, test, build, and deploy"
 	@echo
 	@echo
-	@echo "TODO: UPDATE THIS---->You must set the PROFILE and FUNCTION variables to use the"
-	@echo "deploy target.  For example:"
+	@echo "You must set the FUNCTION variables to use the deploy target."
+	@echo "FUNCTION must be set to the name of an existing lambda function to update."
+	@echo "For example:"
 	@echo
-	@echo "  make PROFILE=foo FUNCTION=bar deploy"
+	@echo "  make deploy FUNCTION=sample-application-staging"
 	@echo
-	@echo "  PROFILE  - the AWS profile to use for the deployment."
-	@echo "  FUNCTION - the name of the existing lambda function to update."
+	@echo "Optional deploy variables are:"
+	@echo "  ENVIRONMENT   - the intended environment for the deployment (default: undefined)"
+	@echo "  VERSION       - the version of the code being deployed (default: undefined)"
+	@echo "  PLATFORM      - the platform being used for the deployment (default: undefined)"
+	@echo "  BUILD_NUMBER  - the build number assigned by the deployment platform (default: undefined)"
+	@echo "  URL           - the URL to use for testing the deployment (default: undefined)"
+	@echo
 
 requirements:
 	pip install -U pip
@@ -38,7 +45,7 @@ check:
 	pylint --version
 	flake8 --version
 	aws --version
-	
+
 lint:
 	pylint --exit-zero --disable=R,C $(CODE)
 	flake8 --exit-zero $(CODE)
@@ -54,21 +61,21 @@ build:
 
 deploy:
 	aws sts get-caller-identity
-	
+
 	aws lambda wait function-active \
 		--function-name="$(FUNCTION)"
-	
+
 	aws lambda update-function-configuration \
 		--function-name="$(FUNCTION)" \
 		--environment "Variables={PLATFORM=$(PLATFORM),ENVIRONMENT=$(ENVIRONMENT),VERSION=$(VERSION),BUILD_NUMBER=$(BUILD_NUMBER)}"
 
 	aws lambda wait function-updated \
 		--function-name="$(FUNCTION)"
-	
+
 	aws lambda update-function-code \
 		--function-name="$(FUNCTION)" \
 	 	--zip-file=fileb://lambda.zip
-	    
+
 	aws lambda wait function-updated \
 		--function-name="$(FUNCTION)"
 
--- index.py
+++ index.py
@@ -1,109 +1,35 @@
+"""
+An API that returns data from a JSON file.
+"""
+import os
+import json
+
+
 def handler(event, context):
-    import os
-    import json
-
-    environment = os.environ.get("ENVIRONMENT", "undefined")
-    platform = os.environ.get("PLATFORM", "undefined")
-    version = os.environ.get("VERSION", "undefined")
-    build_number = os.environ.get("BUILD_NUMBER", "undefined")
-
-    with open("data.json", "r") as f:
-        data = json.load(f)
+    """
+    The lambda handler function.
+    """
+    del context  # Unused
 
     if event["rawPath"] == "/":
-        # Return the home page for the application
-        docs_page = f"""
-        <!DOCTYPE html>
-        <html>
-        <head>
-            <title>The Sample Application - {environment}</title>
-            <style>
-                body {{
-                    font-family: Arial, sans-serif;
-                    line-height: 1.6;
-                    max-width: 800px;
-                    margin: 0 auto;
-                }}
-                h1, h2 {{
-                    color: #333;
-                    border-bottom: 1px solid #ccc;
-                }}
-                p {{
-                    margin-bottom: 16px;
-                }}
-                button {{
-                    background-color: green;
-                    color: white;
-                    font-weight: bold;
-                    padding: 10px 20px;
-                    font-size: 16px;
-                    border: none;
-                    border-radius: 5px;
-                    cursor: pointer;
-                }}
-                table {{
-                width: 50%;
-                border-collapse: collapse;
-                margin-bottom: 20px;
-                }}
+        environment = os.environ.get("ENVIRONMENT", "undefined")
+        platform = os.environ.get("PLATFORM", "undefined")
+        version = os.environ.get("VERSION", "undefined")
+        build_number = os.environ.get("BUILD_NUMBER", "undefined")
 
-                td, th {{
-                padding: 10px;
-                border: 1px solid #ccc;
-                text-align: left;
-                }}
+        # Read the HTML template
+        with open("template.html", mode="r", encoding="utf-8") as template_file:
+            template = template_file.read()
 
-                th {{
-                background-color: #f2f2f2;
-                font-weight: bold;
-                }}
+        # Render the template
+        docs_page = template.format(
+            environment=environment,
+            version=version,
+            platform=platform,
+            build_number=build_number,
+        )
 
-                td:first-child, th:first-child {{
-                font-weight: bold;
-                background-color: #26A69A ;
-                }}
-
-                td b {{
-                font-weight: normal;
-                }}
-            </style>
-        </head>
-        <body>
-            <h1>The Sample Application</h1>
-            <table>
-              <tr>
-                <td>Environment:</td>
-                <td><b>{environment}</b></td>
-              </tr>
-              <tr>
-                <td>Version:</td>
-                <td><b>{version}</b></td>
-              </tr>
-              <tr>
-                <td>Platform:</td>
-                <td><b>{platform}</b></td>
-              </tr>
-              <tr>
-                <td>Build Number</td>
-                <td><b>{build_number}</b></td>
-              </tr>
-            </table>
-
-            <h2>GET /</h2>
-            <p>Returns the documentation page for the application.</p>
-            <p><button onclick="window.open('/')">Try it</button></p>
-
-            <h2>GET /data</h2>
-            <p>Returns all data in JSON format.</p>
-            <p><button onclick="window.open('/data')">Try it</button></p>
-
-            <h2>GET /{{id}}</h2>
-            <p>Returns a specific item by its ID in JSON format.</p>
-            <p><button onclick="window.open('/1')">Try it</button></p>
-        </body>
-        </html>
-        """
-
+        # Return the rendered template
         return {
             "statusCode": 200,
             "headers": {
@@ -112,6 +38,11 @@ def handler(event, context):
             "body": docs_page,
         }
 
+    # Read the data from the JSON file
+    with open("data.json", mode="r", encoding="utf-8") as data_file:
+        data = json.load(data_file)
+
+    # Route /data: return all data
     if event["rawPath"] == "/data":
         return {
             "statusCode": 200,
@@ -121,13 +52,14 @@ def handler(event, context):
             "body": json.dumps(data),
         }
 
-    # Get the id from the path
-    id = event["rawPath"][1:]
+    # Route /data/{item_id}: return a single item
+    # Get the item_id from the event's rawPath
+    item_id = event["rawPath"][1:]
 
-    # Check the id against each item in the data
+    # Check the item_id against each item in the data
     for item in data:
-        # Return the item if the id matches
-        if item["id"] == id:
+        # Return the item if the item_id matches
+        if item["id"] == item_id:
             return {
                 "statusCode": 200,
                 "headers": {
@@ -136,14 +68,36 @@ def handler(event, context):
                 "body": json.dumps(item),
             }
 
-    # Return a 404 if the id doesn't match any item
-    else:
-        return {
-            "statusCode": 404,
-            "headers": {
-                "Content-Type": "application/json",
-            },
-            "body": json.dumps(
-                {"message": f"id {id} not found", "event": event, "id": id}
-            ),
-        }
+    # Return a 404 if the item_id doesn't match any item
+    return {
+        "statusCode": 404,
+        "headers": {
+            "Content-Type": "application/json",
+        },
+        "body": json.dumps(
+            {
+                "message": f"item_id {item_id} not found",
+                "event": event,
+                "item_id": item_id,
+            }
+        ),
+    }
+
+
+def main():
+    """
+    A main function for testing the handler function.
+    """
+    # Simulate an event for testing
+    event = {"rawPath": "/1"}
+    context = None
+
+    # Call the handler function
+    response = handler(event, context)
+
+    # Print the response
+    print(json.dumps(response, indent=2))
+
+
+if __name__ == "__main__":
+    main()
